770e017d961a9c7b8aa94eb0f6704ec0636f54f5
lifeibiren <lifeibiren@qq.com>
2018-11-07 01:57:00 +0800

    decide to use openssl

--- crypto/AES.cpp
+++ crypto/AES.cpp
@@ -1,1 +1,70 @@
 #include "AES.hpp"
+
+#include <openssl/conf.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include <string.h>
+
+
+byte_buffer AES::AES_encrypt(const byte_buffer &buffer)
+{
+    EVP_CIPHER_CTX *ctx;
+    int len;
+    int ciphertext_len;
+    if (!(ctx = EVP_CIPHER_CTX_new()))
+    {
+
+    }
+
+    if (EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key_, iv) != 1)
+    {
+
+    }
+
+    if (EVP_EncryptUpdate(ctx, ciphertext, &len, buffer, buffer.size()) != 1)
+    {
+
+    }
+    ciphertext_len = len;
+
+    if (EVP_EncryptFinal_ex(ctx, ciphertext_len + len, &len))
+    {
+
+    }
+    ciphertext_len += len;
+
+    EVP_CIPHER_CTX_free(ctx);
+
+}
+
+byte_buffer AES::AES_encrypt(const byte_buffer &buffer)
+{
+    EVP_CIPHER_CTX *ctx;
+    int len;
+    int ciphertext_len;
+    if (!(ctx = EVP_CIPHER_CTX_new()))
+    {
+
+    }
+
+    if (EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv) != 1)
+    {
+
+    }
+
+    if (EVP_DecryptUpdate(ctx, ciphertext, &len, buffer, buffer.size()) != 1)
+    {
+
+    }
+    ciphertext_len = len;
+
+    if (EVP_DecryptFinal_ex(ctx, ciphertext_len + len, &len))
+    {
+
+    }
+    ciphertext_len += len;
+
+    EVP_CIPHER_CTX_free(ctx);
+
+
+}
--- crypto/AES.hpp
+++ crypto/AES.hpp
@@ -1,16 +1,37 @@
 #pragma once
 
-#include <openssl/aes.h>
+#include <cinttypes>
+#include <string>
+#include "bytebuffer.hpp"
 
-class AES_key
+struct AES_key
 {
-public:
+    std::string key;
 
+    void to_AES_Key(std::uint8_t keybytes[32])
+    {
+        size_t min = key.size() < 32 ? key.size() : 32;
+        for (size_t i = 0; i < min; i ++)
+        {
+            keybytes[i] = key[i];
+        }
+    }
+};
+
+struct AES_IV
+{
+    std::uint8_t IV[16];
 };
 
 class AES
 {
 public:
-    static void AES_encrypt();
-    static void AES_ecrypt();
+    AES(const AES_key &key, const AES_IV &iv) : key_(key), iv_(iv)
+    {
+    }
+    byte_buffer AES_encrypt(const byte_buffer &buffer);
+    byte_buffer AES_encrypt(const byte_buffer &buffer);
+protected:
+    AES_key key_;
+    AES_IV iv_;
 };
